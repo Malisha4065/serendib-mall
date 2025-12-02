d5d8d0c41d807c9458c404b2e4f6e2c59ba9fca5
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2025-12-02 23:24:25 +0530

    initial commit

---
+++ inventory-service/src/test/java/com/serendibmall/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serendibmall.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
