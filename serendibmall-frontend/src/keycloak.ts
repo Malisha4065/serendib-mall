de55a63d77aa4d34e5e02713cb3eff8ea09c99eb
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2026-02-17 17:23:50 +0530

    feat: Update Keycloak port from 9090 to 9080 across Docker Compose, BFF, and frontend configurations.

--- serendibmall-frontend/src/keycloak.ts
+++ serendibmall-frontend/src/keycloak.ts
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js'
 
 const keycloak = new Keycloak({
-    url: 'http://localhost:9090',
+    url: 'http://localhost:9080',
     realm: 'serendibmall',
     clientId: 'serendibmall-frontend'
 })
